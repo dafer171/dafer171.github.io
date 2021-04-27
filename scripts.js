2dadb20dec501e1976a20efee50479ce4291f67e
dafer171 <daf_171@hotmail.com>
2021-04-27 23:27:48 +0200

    now

--- scripts.js
+++ scripts.js
@@ -1,3 +1,3 @@
 const date = new Date();
 document.querySelector("#currentYear").innerHTML = date.getFullYear();
-document.querySelector("#LastUpdated").innerHTML = document.lastModified;
+document.querySelector("#LastUpdated").textContent = document.lastModified;
