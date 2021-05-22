5136f043cf4ad9f6f0c3ded1ed78e03428fa1c27
dafer171 <daf_171@homtail.com>
2021-05-22 17:42:46 +0200

    panckes

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -13,3 +13,10 @@ window.onresize = () => {
 const date = new Date();
 document.querySelector("#currentYear").innerHTML = date.getFullYear();
 document.querySelector("#LastUpdated").textContent = document.lastModified;
+const pancakes = document.querySelector(".pancakes");
+const dayOfWeek = date.getDay();
+if (dayOfWeek == 5) {
+  pancakes.style.display = "block";
+} else {
+  pancakes.style.display = "none";
+}
