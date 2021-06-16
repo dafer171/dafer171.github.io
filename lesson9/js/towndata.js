af8d99cdb1c9fa693d7c0da946f5a1926b7027f9
dafer171 <daf_171@homtail.com>
2021-06-17 00:09:04 +0200

    index 3

---
+++ lesson9/js/towndata.js
@@ -0,0 +1,46 @@
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const towns = jsonObject["towns"];
+
+    for (let i = 0; i < towns.length; i = i + 2) {
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let motto = document.createElement("p");
+      let yearFounded = document.createElement("h4");
+      let currentPopulation = document.createElement("h4");
+      let averageRainfall = document.createElement("h4");
+      let photo = document.createElement("img");
+
+      h2.textContent = towns[i].name;
+      card.appendChild(h2);
+
+      motto.textContent = towns[i].motto;
+      card.appendChild(motto);
+
+      yearFounded.textContent = "Year Founded: " + towns[i].yearFounded;
+      card.appendChild(yearFounded);
+
+      currentPopulation.textContent =
+        "Population: " + towns[i].currentPopulation;
+      card.appendChild(currentPopulation);
+
+      averageRainfall.textContent =
+        "Annual Rain Fall: " + towns[i].averageRainfall;
+      card.appendChild(averageRainfall);
+
+      photo.setAttribute("src", towns[i].photo);
+      card.appendChild(photo);
+
+      photo.setAttribute("alt", towns[i].name);
+      card.appendChild(photo);
+
+      document.querySelector("div.cards").appendChild(card);
+    }
+    console.table(towns);
+  });
