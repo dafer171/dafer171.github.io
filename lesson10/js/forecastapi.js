3b11b0f7fd8b144a29e3003469d737c5a43a10a7
dafer171 <daf_171@homtail.com>
2021-06-29 11:52:42 +0200

    forescast

--- lesson10/js/forecastapi.js
+++ lesson10/js/forecastapi.js
@@ -4,6 +4,56 @@ fetch(foreURL)
   .then((response) => response.json())
   .then((jsObject) => {
     //console.log(jsObject);
+    const dayofWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const list = jsObject.list.filter((list) =>
+      list.dt_txt.includes("18:00:00")
+    );
+    console.log(list);
+
+    for (let i = 0; i < list.length; i++) {
+      let d = new Date(list[i].dt_txt);
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let h3 = document.createElement("h3");
+      //let place = document.createElement("h3")
+      let image = document.createElement("img");
+
+      h2.textContent = dayofWeek[d.getDay()];
+      card.appendChild(h2);
+      //console.log(h2);
+
+      //h2.textContent = prophets[i].name + " " + prophets[i].lastname;
+      //card.appendChild(h2);
+
+      h3.textContent = list[i].main.temp.toFixed(0);
+      card.appendChild(h3);
+
+      //place.textContent =
+      //"Place of birth: " +
+      //prophets[i].birthplace;
+      //card.appendChild(place);
+
+      image.setAttribute(
+        "src",
+        "https://openweathermap.org/img/w/" + list[i].weather[0].icon + ".png"
+      );
+      card.appendChild(image);
+
+      //image.setAttribute("alt", prophets[i].name + " " + prophets[i].lastname +  " - " + prophets[i].order);
+      //card.appendChild(image);
+
+      document.querySelector("div.forecast").appendChild(card);
+    }
+    //console.table(list);
+  });
+
+/*
+const foreURL =
+  "https://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=33033af6ab98d426efd2c19223b9729c&units=imperial";
+fetch(foreURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
 
     const list = jsObject.list.filter((list) =>
       list.dt_txt.includes("18:00:00")
@@ -35,3 +85,5 @@ fetch(foreURL)
       "https://openweathermap.org/img/w/" + list[4].weather[0].icon + ".png";
     document.getElementById("icon5").setAttribute("src", imagesrc5);
   });
+
+  */
