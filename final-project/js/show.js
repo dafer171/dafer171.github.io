f23aea20d3f658f030183bfe8f632824913d9b4d
dafer171 <daf_171@homtail.com>
2021-07-19 22:26:08 +0200

    initial

---
+++ final-project/js/show.js
@@ -0,0 +1,11 @@
+const show = "david";
+let string = "me voy a ganar el premio mayor";
+console.log(`voy de camnino a 
+${string}`);
+
+const dayName = "sabado";
+const monthName = "julio";
+const year = 2021; 
+const d = new Date();
+const fulldate = `${ dayName }  ${ d.getDate() } ${ monthName }  ${ year}`;
+console.log(fulldate);
