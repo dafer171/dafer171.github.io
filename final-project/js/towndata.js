a059aa1cfbaeb0a0782285e3de636909399a920b
dafer171 <daf_171@homtail.com>
2021-07-20 12:15:46 +0200

    towndata local

--- final-project/js/towndata.js
+++ final-project/js/towndata.js
@@ -1,4 +1,6 @@
-const requestURL = "https://github.com/dafer171/dafer171.github.io/blob/master/final-project/json/towndata.json";
+const requestURL = "http://127.0.0.1:5500/final-project/json/towndata.json"
+//"https://byui-cit230.github.io/weather/data/towndata.json";
+//"https://github.com/dafer171/dafer171.github.io/blob/master/final-project/json/towndata.json";
 
 fetch(requestURL)
   .then(function (response) {
